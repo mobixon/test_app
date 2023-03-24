e56c5bf3459d8618274f2f94bf593ec5d81f0acc
Oleksandr Stoilovskyi <79860274+oleksandr-stoilovskyi@users.noreply.github.com>
2023-03-24 20:41:05 +0200

    Extended tests for round check

--- tests/Model/TransactionCommissionCalculatorTest.php
+++ tests/Model/TransactionCommissionCalculatorTest.php
@@ -16,11 +16,11 @@ class TransactionCommissionCalculatorTest extends TestCase
     /**
      * @dataProvider calculateDataProvider
      */
-    public function testCalculate($transaction, $countryIso2Code, $expected)
+    public function testCalculate($transaction, $countryIso2Code, $rate, $expected)
     {
         //arrange
         $exchangeProvider = $this->createMock(CurrencyExchangeProviderInterface::class);
-        $exchangeProvider->method('getExchangeRate')->willReturn(0.5);
+        $exchangeProvider->method('getExchangeRate')->willReturn((float) $rate);
 
         $cardBinDataProvider = $this->createMock(CardBinDataProviderInterface::class);
         $cardBinDataProvider->method('getCountryIso2CodeByBin')->willReturn($countryIso2Code);
@@ -43,11 +43,14 @@ class TransactionCommissionCalculatorTest extends TestCase
     public static function calculateDataProvider(): array
     {
         return [
-            [new TransactionDataTransfer("41417360", 100, 'EUR'), 'AT', 1],
-            [new TransactionDataTransfer("51417360", 400, 'USD'), 'US', 16],
-            [new TransactionDataTransfer("51417360", 400, 'USD'), 'DE', 8],
-            [new TransactionDataTransfer("41417360", 200, 'GBP'), 'AT', 4],
-            [new TransactionDataTransfer("41417360", 300, 'EUR'), 'AT', 3],
+            [new TransactionDataTransfer("41417360", 100, 'EUR'), 'AT', 0.5, 1],
+            [new TransactionDataTransfer("51417360", 400, 'USD'), 'US', 0.5, 16],
+            [new TransactionDataTransfer("51417360", 400, 'USD'), 'DE', 0.5, 8],
+            [new TransactionDataTransfer("41417360", 200, 'GBP'), 'AT', 0.5, 4],
+            [new TransactionDataTransfer("41417360", 300, 'EUR'), 'AT', 1, 3],
+            [new TransactionDataTransfer("51417360", 400, 'USD'), 'US', 1.5, 5.34],
+            [new TransactionDataTransfer("51417360", 400, 'USD'), 'DE', 1.5, 2.67],
+            [new TransactionDataTransfer("41417360", 200, 'GBP'), 'AT', 0.5, 4],
         ];
     }
 }
